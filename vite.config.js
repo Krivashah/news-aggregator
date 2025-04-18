fd0627b264cbb5070906466d4684d4fb4c3bd790
KRIVA <krivayshah@gmail.com>
2025-04-18 15:05:28 +0530

    Update vite.config.js to allow Render host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Allowing connection from any host
+  },
+  preview: {
+    allowedHosts: ['news-aggregator-8uta.onrender.com'], // Allowing the specific host
+  }
+});
